ab773c4a3c3d8fa4829aab4332b8e2f7f7196443
konradUllrich <141348970+konradUllrich@users.noreply.github.com>
2024-04-28 12:20:58 +0200

    adding first test

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("bam");
+const errors = [];
+
+errors.push({
+  title: "should have exactly 1 <h1>",
+  message: `found ${document.querySelectorAll("h1").length}`,
+});
+
+errors.forEach((e) => {
+  console.log(e.message);
+  console.log("----");
+});
