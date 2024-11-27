81376d34e2079e40fdc5bef75d78ac0a8c630250
MelanieKingMoysies <melanie.king@moysies.de>
2024-11-27 11:14:24 +0100

    Update main.js
    Add Link

--- main.js
+++ main.js
@@ -383,7 +383,7 @@ class TestManager {
           state: failingElements.length === 0,
           elements: failingElements,
           selector,
-          links: [{ label: "", url: "" }],
+          links: [{ label: "Loop: aria-activedescendant ", url: "https://moysies.sharepoint.com/:fl:/g/contentstorage/CSP_de9f239b-8aef-4eff-9988-df21952d00e6/EYQfTD8WZd5MsT7BI_Zfu2kBwB4YpUIU4k6yr6hx2IABQw?e=cO1WlQ&nav=cz0lMkZjb250ZW50c3RvcmFnZSUyRkNTUF9kZTlmMjM5Yi04YWVmLTRlZmYtOTk4OC1kZjIxOTUyZDAwZTYmZD1iJTIxbXlPZjN1LUtfMDZaaU44aGxTMEE1b01EWnk0dlVqQkppVGhaZDhiM1dtU1BJUFlFaDhjalFZa3ItWkxlWUxZaCZmPTAxQUo2Ulc3TUVENUdENkZURjNaR0xDUFdCRVAzRjdPM0omYz0lMkYmYT1Mb29wQXBwJnA9JTQwZmx1aWR4JTJGbG9vcC1wYWdlLWNvbnRhaW5lciZ4PSU3QiUyMnclMjIlM0ElMjJUMFJUVUh4dGIzbHphV1Z6TG5Ob1lYSmxjRzlwYm5RdVkyOXRmR0loYlhsUFpqTjFMVXRmTURaYWFVNDRhR3hUTUVFMWIwMUVXbmswZGxWcVFrcHBWR2hhWkRoaU0xZHRVMUJKVUZsRmFEaGphbEZaYTNJdFdreGxXVXhaYUh3d01VRktObEpYTjBsTFJFRk5VVkZVVTA5U1ZrTkpWekpYTkZWUFIwTlRNalpDJTIyJTJDJTIyaSUyMiUzQSUyMmJjNmY5N2M4LTM2MTctNGI1Ni1iZjhkLTc3ZWI2Y2ZlZGVjNiUyMiU3RA%3D%3D" }],
         };
       },
     },
