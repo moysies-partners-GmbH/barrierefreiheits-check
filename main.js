98ebfbf4d2717a1ac7f2821f1de8028edbd2a54c
MelanieKingMoysies <melanie.king@moysies.de>
2024-11-27 11:17:14 +0100

    Update main.js
    Add Link

--- main.js
+++ main.js
@@ -312,7 +312,7 @@ class TestManager {
           state: faultyElements.length === 0,
           elements: faultyElements,
           selector,
-          links: [{ label: "", url: "" }],
+          links: [{ label: "Loop: Label Elemente", url: "https://moysies.sharepoint.com/:fl:/g/contentstorage/CSP_de9f239b-8aef-4eff-9988-df21952d00e6/EXY-0S7IwJBFqoyHOyu_WlcBu9Nl0-mGjiriQRkaxUWXrw?e=6ikCpY&nav=cz0lMkZjb250ZW50c3RvcmFnZSUyRkNTUF9kZTlmMjM5Yi04YWVmLTRlZmYtOTk4OC1kZjIxOTUyZDAwZTYmZD1iJTIxbXlPZjN1LUtfMDZaaU44aGxTMEE1b01EWnk0dlVqQkppVGhaZDhiM1dtU1BJUFlFaDhjalFZa3ItWkxlWUxZaCZmPTAxQUo2Ulc3TFdIM0lTNVNHQVNCQzJWREVISE1WMzZXU1gmYz0lMkYmYT1Mb29wQXBwJng9JTdCJTIydyUyMiUzQSUyMlQwUlRVSHh0YjNsemFXVnpMbk5vWVhKbGNHOXBiblF1WTI5dGZHSWhiWGxQWmpOMUxVdGZNRFphYVU0NGFHeFRNRUUxYjAxRVduazBkbFZxUWtwcFZHaGFaRGhpTTFkdFUxQkpVRmxGYURoamFsRlphM0l0V2t4bFdVeFphSHd3TVVGS05sSlhOMGxMUkVGTlVWRlVVMDlTVmtOSlZ6SlhORlZQUjBOVE1qWkMlMjIlMkMlMjJpJTIyJTNBJTIyNGU2ZjVhZjYtNDcwZi00NmRhLWFkNGMtZmY0NDkzY2RkNTM0JTIyJTdE" }],
         };
       },
     },
